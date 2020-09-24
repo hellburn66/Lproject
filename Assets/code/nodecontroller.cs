a00906177a10956f8459daabce4b3e83825d58ed
helldrew66 <sfeaste1@email.cpcc.edu>
2020-09-24 14:34:57 -0400

    added directional node logic

--- Assets/code/nodecontroller.cs
+++ Assets/code/nodecontroller.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class nodecontroller : MonoBehaviour
 {
+    SpriteRenderer sr;
+    public Sprite none;
+    public Sprite left;
+    public Sprite right;
+    public string current;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = left;
+        SwitchSprite();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void SwitchSprite()
+    {
+        if (current == "left")
+        {
+            Debug.Log("Choose Left");
+            sr.sprite = left;
+        }
+        else if (current == "right")
+        {
+            Debug.Log("Choose Right");
+            sr.sprite = right;
+        }
+        else
+        {
+            Debug.Log("Choose None");
+            sr.sprite = none;
+        }
+
     }
 }
