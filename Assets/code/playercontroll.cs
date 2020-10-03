f2bb3414c6c4b8e4d3bcdfadfb093295cdf7372a
helldrew66 <sfeaste1@email.cpcc.edu>
2020-10-03 11:12:03 -0400

    update assets

---
+++ Assets/code/playercontroll.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroll : MonoBehaviour
+{
+    public float speed;
+    public float startDirection;
+    public bool right;
+    void Start()
+    {
+        if (!right)
+        {
+            transform.Rotate(Vector3.back * 180);
+        }
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right *speed);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       
+       nodecontroller dn = other.GetComponent<nodecontroller>();
+        Debug.Log(dn.current);
+        //check direction
+        float rot = 0;
+        Vector3 dir = Vector3.zero;
+        if (dn.current == "right")
+        {
+            dir = Vector3.back;
+            rot = 90;
+        }
+        else if (dn.current == "left")
+        {
+            dir = Vector3.forward;
+            rot = 90;
+        }
+
+        transform.Rotate(dir * rot);//back is right, forward is left;
+    }
+
+}
